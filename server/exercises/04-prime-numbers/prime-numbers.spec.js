badaaf085613a27a49a65cad095300128b328841
Ya-Ching Tang <yachingtang2@gmail.com>
2018-12-11 14:18:20 -0600

    Prime number unit tests.

--- server/exercises/04-prime-numbers/prime-numbers.spec.js
+++ server/exercises/04-prime-numbers/prime-numbers.spec.js
@@ -1,1 +1,14 @@
-/* eslint-disable no-param-reassign */
+describe('Verify if a number is a Prime number', () => {
+   it('0 is not');
+   it('1 is');
+   it('2 is');
+   it('3 is');
+   it('4 is not');
+   it( '5 is');
+   it('6 is not');
+   it('7 is');
+   it('8 is not');
+   it('9 is not');
+   it('10 is not');
+   it('11 is');
+});
