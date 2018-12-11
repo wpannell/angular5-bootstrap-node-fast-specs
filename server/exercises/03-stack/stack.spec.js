f68224e29bbdbf169491fbb9ab4972a1a5a228bd
Ya-Ching Tang <yachingtang2@gmail.com>
2018-12-11 11:24:38 -0600

    Stack design.

---
+++ server/exercises/03-stack/stack.spec.js
@@ -0,0 +1,12 @@
+describe('stack', () => {
+  it('starts empty');
+  it('starts with a length of zero');
+  it('when push is not empty');
+  it('when push length is one');
+  it('should pop the same that is pushed');
+  it('length should be zero after push and pop');
+  it('push two and pop returns the last element pushed');
+  it('push two, 2nd pop returns the first element pushed');
+  it('pop should throw a stack empty error when empty');
+  it('push should throw a stack at capacity error when at capacity');
+});
