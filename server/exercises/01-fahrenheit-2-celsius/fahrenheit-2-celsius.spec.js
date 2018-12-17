20bf2dead6da6ebec9583a8bbe24a0963e264fd6
WIL PANNELL <wgpannel@us.ibm.com>
2018-12-17 16:44:32 -0600

    design fahrenheit to celsius feature

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,15 +1,9 @@
-let fahrenheit2celsius = temp => (temp - 32) * 5 / 9;
-
 describe('a fahrenheit2celsius feature', () => {
-  it('has a working test infrastructure', () => {
-    true.should.be.true();
+  it('shows the automated test infrastructure works', () => {
+    true.should.be.true('this is a customized message');
   });
-
-  it('fahrenheit2celsius of 212 is 100', () => {
-    fahrenheit2celsius(212).should.equal(100);
-  });
-
-  it('fahrenheit2celsius of 32 is 0', () => {
-    fahrenheit2celsius(32).should.equal(0);
-  });
+  
+  it('212° fahrenheit is 100° celsius');
+  it('32° fahrenheit is 0° celsius');
+  it('122° fahrenheit is 50° celsius');
 });
