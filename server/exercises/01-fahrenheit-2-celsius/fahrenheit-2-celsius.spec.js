deaabd4253508d54bc56fbf4a866fad37da70a20
AA Developer <developer@aa.com>
2018-12-17 16:59:54 -0600

    FahrenheitToCelsius API - 32 degrees fahrenheit is 0 degree celsius.

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,9 +1,14 @@
+let fahrenheitToCelsius = () => 0;
+
 describe('a fahrenheit2celsius feature', () => {
   it('shows the automated test infrastructure works', () => {
     true.should.be.true('a customized message');
   });
 
-  it('32 degrees fahrenheit is 0 degree celsius');
+  it('32 degrees fahrenheit is 0 degree celsius', () => {
+    fahrenheitToCelsius(32).should.equal(0);
+  });
+
   it('212 degrees fahrenheit is 100 degrees celsius');
   it('122 degrees fahrenheit is 50 degrees celsius');
 });
