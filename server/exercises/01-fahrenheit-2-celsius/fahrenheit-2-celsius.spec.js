e089a3e141937bb32ee131d1120601d4580fe842
AA Developer <developer@aa.com>
2018-12-17 17:02:23 -0600

    kernel test defined the api and first test

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,8 +1,16 @@
+function fahrenheitToCelsius(number) {
+  return 100;
+}
+
 describe('the fahrenheit2celsius canary spec', () => {
   it('shows the test infrastructure works', () => {
     true.should.be.true('test not working');
   });
-  it('212° fahrenheit = 100° celsius');
+  it('212° fahrenheit = 100° celsius', () => {
+    fahrenheitToCelsius(212).should.equal(100);
+  });
+
+
   it('32° fahrenheit = 0° celsius');
   it('122° fahrenheit = 50° celsius');
 });
