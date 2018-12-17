8db3cc7cdbd252bf9c8d9946a9d1cbed63c53a06
AA Developer <developer@aa.com>
2018-12-17 17:01:07 -0600

    first test to convert fahrenheit 212

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,9 +1,13 @@
+let fahrenheit2Celsius = () => 100;
+
 describe('fahrenheit2celsius feature', () => {
   it('shows the automated test infrastructure works', () => {
     true.should.be.true('this is a customized message for farhenheit 2 celsius feature.');
   });
 
-  it('212° fahrenheit in celsius is 100°');
+  it('212° fahrenheit in celsius is 100°', () => {
+    fahrenheit2Celsius(212).should.equal(100);
+  });
   it('32° fahrenheit in celsius is 0°');
   it('122° fahrenheit in celsius is 50°');
 
