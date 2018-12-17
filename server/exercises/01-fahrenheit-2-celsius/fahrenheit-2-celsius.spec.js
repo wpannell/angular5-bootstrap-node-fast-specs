aa94b4eb94ec683a7981e47898a0718bce7a6c8a
AA Developer <developer@aa.com>
2018-12-17 16:47:36 -0600

    design fahrenheit to celsius feature.

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -2,4 +2,8 @@ describe('a fahrenheit2celsius feature', () => {
   it('shows the automated test infrastructure works', () => {
     true.should.be.true('a customized message');
   });
+
+  it('32 degrees fahrenheit is 0 degree celsius');
+  it('212 degrees fahrenheit is 100 degrees celsius');
+  it('122 degrees fahrenheit is 50 degrees celsius');
 });
