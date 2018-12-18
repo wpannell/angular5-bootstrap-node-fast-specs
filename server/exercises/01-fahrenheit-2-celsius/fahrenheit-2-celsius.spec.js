285fa0c4c86fbde8cba1e0d2ca731aeaa7a97a26
AA Developer <developer@aa.com>
2018-12-18 09:51:59 -0600

    red bar expect 100 to be 0

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -8,7 +8,9 @@ describe('fahrenheit2celsius feature', () => {
   it('212° fahrenheit in celsius is 100°', () => {
     fahrenheit2Celsius(212).should.equal(100);
   });
-  it('32° fahrenheit in celsius is 0°');
+  it('32° fahrenheit in celsius is 0°', () => {
+              fahrenheit2Celsius(32).should.equal(0);
+  });
   it('122° fahrenheit in celsius is 50°');
 
 
