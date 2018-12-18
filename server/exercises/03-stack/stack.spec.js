2ba74dd014bc1f6acd1361c5691fceec225a5fbb
AA Developer <developer@aa.com>
2018-12-18 13:32:58 -0600

    Stack design spec.

---
+++ server/exercises/03-stack/stack.spec.js
@@ -0,0 +1,8 @@
+describe('stack implementation', () => {
+  it('push nothing - stack should be empty');
+  it('push one element - stack should have one element');
+  it('push one element, pop one element - popped element is the pushed element');
+  it('push two elements, popped two elements - last popped element should be the first pushed element');
+  it('push two elements, peek should return the second pushed element');
+  it('pop an element when stack is empty - should returns an error');
+});
