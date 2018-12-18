b8aaffdde6976b9d6566e55e6bdd90156d1fbded
AA Developer <developer@aa.com>
2018-12-18 13:33:10 -0600

    stack specs

--- server/exercises/02-palindrome/palindrome.spec.js
+++ server/exercises/02-palindrome/palindrome.spec.js
@@ -1,4 +1,4 @@
-let isPalindrome = (text) => text === 'mom';
+let isPalindrome = (text) => (text.split('').reverse().join('') === text);
 
 describe('a palindrome function', () => {
   it('has a canary test that proves the test infrastructure works', () => {
@@ -12,6 +12,14 @@ describe('a palindrome function', () => {
   it('mom and dad is not palindrome', () => {
     isPalindrome('mom and dad').should.be.false();
   });
-  it('madam is a palindrome');
-  it('rise to vote sir is a palindrome');
+  it('madam is a palindrome', () => {
+    isPalindrome('madam').should.be.true();
+
+  });
+
+
+  it('rise to vote sir is not a palindrome', () => {
+    console.log(isPalindrome('rise to vote sir is a palindrome'));
+    isPalindrome('rise to vote sir is a palindrome').should.be.false();
+  });
 });
