dd510aff7fd313e224c8e2a08c1b30dfd1dec014
AA Developer <developer@aa.com>
2018-12-18 15:12:59 -0600

    Stack first 5 tests green

--- server/exercises/03-stack/stack.spec.js
+++ server/exercises/03-stack/stack.spec.js
@@ -1,9 +1,53 @@
-describe('', ()=>{
-  it('starts empty');
-  it('starts with a length of zero');
-  it('when push is not empty');
-  it('when push length is one');
-  it('should pop the same that is pushed');
+class Stack {
+
+  constructor() {
+    this.items = [];
+  }
+
+  isEmpty() {
+    return !this.items || this.items.length === 0;
+  }
+
+  length() {
+    return this.items.length;
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+}
+
+describe('', () => {
+  it('starts empty', () => {
+    let stack = new Stack();
+    stack.isEmpty().should.be.true();
+  });
+  it('starts with a length of zero', () => {
+    let stack = new Stack();
+    stack.length().should.equal(0);
+  });
+  it('when push is not empty', () => {
+    let stack = new Stack();
+    stack.push(1);
+    stack.isEmpty().should.be.false();
+  });
+
+  it('when push length is one', () => {
+    let stack = new Stack();
+    stack.push(1);
+    stack.length().should.equal(1);
+  });
+
+  it('should pop the same that is pushed', () => {
+    let stack = new Stack();
+    stack.push(1);
+    stack.pop().should.be.equal(1);
+  });
+
   it('length should be zero after push and pop');
   it('push two and pop returns the last element pushed');
   it('push two, 2nd pop returns the first element pushed');
