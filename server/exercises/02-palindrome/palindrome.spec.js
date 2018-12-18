ec847ab76067d2916c5c29db889b541281ad0b28
AA Developer <developer@aa.com>
2018-12-18 11:57:42 -0600

    add implementation for test 3 to run green and apply 4 rules

--- server/exercises/02-palindrome/palindrome.spec.js
+++ server/exercises/02-palindrome/palindrome.spec.js
@@ -1,16 +1,21 @@
-let palindrome = (phrase) => phrase === 'mom';
-describe('a palindrome function', () => {
+let isPalindrome = phrase => phrase.split('').reverse().join('') === phrase;
+
+describe('a isPalindrome function', () => {
   it('has a canary test that proves the test infrastructure works', () => {
     true.should.be.true();
   });
 
-  it('mom is a palindrome', () => {
-    palindrome('mom').should.be.true();
+  it('mom is a isPalindrome', () => {
+    isPalindrome('mom').should.be.true();
   });
 
-  it('mom and dad is not palindrome', () => {
-    palindrome('mom and dad').should.be.false();
+  it('mom and dad is not isPalindrome', () => {
+    isPalindrome('mom and dad').should.be.false();
   });
-  it('madam is a palindrome');
-  it('rise to vote sir is a palindrome');
+
+  it('madam is a isPalindrome', () => {
+    isPalindrome('madam').should.be.true();
+  });
+
+  it('rise to vote sir is a isPalindrome');
 });
