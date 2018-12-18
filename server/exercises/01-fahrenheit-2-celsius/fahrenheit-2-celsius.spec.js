f6536fe76583dd52aea793dc0f713dbf087cdde7
WIL PANNELL <wgpannel@us.ibm.com>
2018-12-18 09:46:51 -0600

    design api

--- server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -2,8 +2,11 @@ describe('a fahrenheit2celsius feature', () => {
   it('shows the automated test infrastructure works', () => {
     true.should.be.true('this is a customized message');
   });
-  
-  it('212° fahrenheit is 100° celsius');
+
+  it('212° fahrenheit is 100° celsius', () => {
+    fahrenheit2Celsius(212).should.equal(100);
+  });
+
   it('32° fahrenheit is 0° celsius');
   it('122° fahrenheit is 50° celsius');
 });
