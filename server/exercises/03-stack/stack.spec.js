227141557cc8149330f93573ff62bde7e455b817
AA Developer <developer@aa.com>
2018-12-18 14:17:23 -0600

    Stack length is one.

--- server/exercises/03-stack/stack.spec.js
+++ server/exercises/03-stack/stack.spec.js
@@ -1,14 +1,24 @@
+let elements = [];
+
 let stack = {
   isEmpty() {
-    return true;
+    return elements.length === 0;
   },
 
   length() {
-    return 0;
+    return elements.length;
+  },
+
+  push(element) {
+    elements.unshift(element);
   }
 };
 
 describe('stack implementation', () => {
+  beforeEach(() => {
+    elements = [];
+  });
+
   it('shows the automated test infrastructure works', () => {
     true.should.be.true();
   });
@@ -21,8 +31,16 @@ describe('stack implementation', () => {
     stack.length().should.equal(0);
   });
 
-  it('when push is not empty');
-  it('when push length is one');
+  it('when push is not empty', () => {
+    stack.push(1);
+    stack.isEmpty().should.be.false();
+  });
+
+  it('when push length is one', () => {
+    stack.push(2);
+    stack.length().should.equal(1);
+  });
+
   it('should pop the same that is pushed');
   it('length should be zero after push and pop');
   it('push two and pop returns the last element pushed');
