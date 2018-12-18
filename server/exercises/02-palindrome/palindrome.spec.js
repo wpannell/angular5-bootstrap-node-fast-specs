0fed9da2216dbd4b31df131540dac5e7338caf90
AA Developer <developer@aa.com>
2018-12-18 11:28:13 -0600

    Mom is a palindrome.

--- server/exercises/02-palindrome/palindrome.spec.js
+++ server/exercises/02-palindrome/palindrome.spec.js
@@ -1,9 +1,16 @@
+function isPalindrome(word) {
+  return true;
+}
+
 describe('a palindrome function', () => {
   it('has a canary test that proves the test infrastructure works', () => {
     true.should.be.true();
   });
 
-  it('mom is a palindrome');
+  it('mom is a palindrome', () => {
+    isPalindrome('mom').should.be.true();
+  });
+
   it('mom and dad is not palindrome');
   it('madam is a palindrome');
   it('rise to vote sir is a palindrome');
