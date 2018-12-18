911ac1b25538e593c562bdad06e6d617598a5b82
AA Developer <developer@aa.com>
2018-12-18 13:49:31 -0600

    stack design specs

--- server/exercises/03-stack/stack.spec.js
+++ server/exercises/03-stack/stack.spec.js
@@ -2,8 +2,8 @@ describe('implement stack', function () {
 
   it('stack to be empty');
   it('pop call on empty stack to throw stack empty exception');
-    it('push elements into stack 10');
-  it('pop should return 10');
-  it('peak should return 10');
+  it('push elements into stack 10');
+  it('push 10 should pop  10');
+  it('push 10 on peak should return 10');
 
 });
