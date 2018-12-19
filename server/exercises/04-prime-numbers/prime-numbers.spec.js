b66152cd242f7b572f3feeb1785f41ad46a4d9ad
WIL PANNELL <wgpannel@us.ibm.com>
2018-12-19 10:57:02 -0600

    leave the rest as exercise

--- server/exercises/04-prime-numbers/prime-numbers.spec.js
+++ server/exercises/04-prime-numbers/prime-numbers.spec.js
@@ -1,18 +1,34 @@
 /* eslint-disable no-param-reassign */
 
-let primeFactorsOf = number => (number === 1) ? [1] : [2];
+let primeFactorsOf = number => {
+  const factors = [];
+  if (number > 1) {
+    if ((number % 2) === 0) {
+      factors.push(2);
+      number /= 2;
+    }
+    if (number > 1) factors.push(number);
+  }
+  return factors;
+};
 
 describe('prime factors', () => {
-  it('1 = [1]', () => {
-    primeFactorsOf(1).should.deepEqual([1]);
+  it('1 = []', () => {
+    primeFactorsOf(1).should.deepEqual([]);
   });
 
   it('2 = [2]', () => {
     primeFactorsOf(2).should.deepEqual([2]);
   });
 
-  it('3 = [3]');
-  it('4 = [2, 2]');
+  it('3 = [3]', () => {
+     primeFactorsOf(3).should.deepEqual([3]);
+  });
+
+  it('4 = [2, 2]', () => {
+     primeFactorsOf(4).should.deepEqual([2, 2]);
+
+  });
   it('5 = [5]');
   it('6 = [2, 3]');
   it('7 = [7]');
