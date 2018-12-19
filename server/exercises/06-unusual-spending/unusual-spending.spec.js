cf17f395aaea0b0b14db2e9168c89bdb30404d09
AA Developer <developer@aa.com>
2018-12-19 11:49:33 -0600

    design spec for unusual spending

---
+++ server/exercises/06-unusual-spending/unusual-spending.spec.js
@@ -0,0 +1,9 @@
+describe.only('unusual spending', () => {
+  it('has a canary spec that shows test automation works', () => {
+    true.should.be.true('this is a customized message');
+  });
+
+  it('unusal spending manages the collaboration of fetch, ' +
+    'categorize and email');
+});
+
