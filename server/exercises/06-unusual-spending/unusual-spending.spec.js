d20c8dc5754a9cbbe71f1c2ca0b487e2b8b6dae6
AA Developer <developer@aa.com>
2018-12-19 11:45:33 -0600

    Update unusal spending canary test's description.

--- server/exercises/06-unusual-spending/unusual-spending.spec.js
+++ server/exercises/06-unusual-spending/unusual-spending.spec.js
@@ -1,5 +1,5 @@
-describe('Unusual spending', () => {
-  it('shows the test infrastructure works', () => {
+describe.only('unusual spending', () => {
+  it('has a canary spec that shows test automation works', () => {
     true.should.be.true('a customized message');
   });
 });
