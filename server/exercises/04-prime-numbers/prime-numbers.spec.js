0a07ffddccdc9f13fae821dcc16a926208bc06a2
AA Developer <developer@aa.com>
2018-12-19 12:03:34 -0600

    prime factors test 1 green

--- server/exercises/04-prime-numbers/prime-numbers.spec.js
+++ server/exercises/04-prime-numbers/prime-numbers.spec.js
@@ -1,1 +1,17 @@
+let primeFactorsOf = number => [];
+
 /* eslint-disable no-param-reassign */
+describe('prime factors', () => {
+  it('1=[1]', () => {
+    primeFactorsOf(1).should.deepEqual([]);
+  });
+  it('2=[1]');
+  it('3=[3]');
+  it('4=[2,2]');
+  it('5=[5]');
+  it('6=[2,3]');
+  it('7=[7]');
+  it('8=[2,2,2]');
+  it('9=[3,3]');
+
+});
