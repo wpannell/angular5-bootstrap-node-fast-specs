b60efb78c55319f9a75b92988cefa377f7c4f18b
AA Developer <developer@aa.com>
2018-12-19 09:18:29 -0600

    Prime factors design spec.

--- server/exercises/04-prime-numbers/prime-numbers.spec.js
+++ server/exercises/04-prime-numbers/prime-numbers.spec.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-param-reassign */
+describe.only('Prime factors', () => {
+  it('of 1 is 1');
+  it('of 2 is 2');
+  it('of 3 is 3');
+  it('of 4 is 2 and 2');
+  it('of 6 is 2 and 3');
+});
