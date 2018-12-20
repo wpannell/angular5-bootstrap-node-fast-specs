79abd6915ee27041642b49e527bb78b0a32fdb77
AA Developer <developer@aa.com>
2018-12-20 08:17:25 -0600

    prime numbers in progress

--- server/exercises/04-prime-numbers/prime-numbers.spec.js
+++ server/exercises/04-prime-numbers/prime-numbers.spec.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-param-reassign */
 let primeFactorsOf = number => {
   const factors = [];
-  if (number > 1) {
+  while (number > 1) {
     if ((number % 2) === 0) {
       factors.push(2);
       number /= 2;
     }
-    if (number > 1) {
-      factors.push(number);
-    }
+  }
+  if (number > 1) {
+     factors.push(number);
   }
   return factors;
 };
@@ -30,10 +30,22 @@ describe('prime factors', () => {
     primeFactorsOf(4).should.deepEqual([2, 2]);
   });
 
-  it('5=[5]');
-  it('6=[2,3]');
-  it('7=[7]');
-  it('8=[2,2,2]');
+  it('5=[5]', () => {
+    primeFactorsOf(5).should.deepEqual([5]);
+  });
+
+  it('6=[2,3]', () => {
+    primeFactorsOf(6).should.deepEqual([2, 3]);
+  });
+
+  it('7=[7]', () => {
+    primeFactorsOf(7).should.deepEqual([7]);
+  });
+
+  it('8=[2,2,2]', () => {
+    primeFactorsOf(8).should.deepEqual([2, 1, 2]);
+  });
+
   it('9=[3,3]');
 
 });
