a04c9f1963cae1311b0cc860b3d69f628717dafc
AA Developer <developer@aa.com>
2018-12-20 08:18:18 -0600

    unusual spendings collaborators

--- server/exercises/06-unusual-spending/unusual-spending.spec.js
+++ server/exercises/06-unusual-spending/unusual-spending.spec.js
@@ -1,9 +1,37 @@
+import td from 'testdouble';
+
+const {replace, when, verify} = td;
+const path = './server/exercises/06-unusual-spending/';
+
 describe.only('unusual spending', () => {
-  it('has a canary spec that shows test automation works', () => {
-    true.should.be.true('this is a customized message');
+  it('unusal spending manages the collaboration of fetch, ' +
+    'categorize and email', () => {
+
+    // === arrange ===
+    const fetch = replace('../fetch').fetch;
+    const categorize = replace(path + 'categorize').categorize;
+    const email = replace(path + 'email').email;
+
+    // system under test
+    let unusualSpending;
+
+    const userId = 'userId';
+    const payments = 'payments';
+    const categorizedPayments = 'categorizedPayments';
+
+    when(fetch(userId)).thenReturn(payments);
+    when(categorize(payments)).thenReturn(categorizedPayments);
+
+    unusualSpending = require('./unusual-spending').unusualSpending;
+
+    // act // when
+    unusualSpending(userId);
+
+    // assert //then
+    verify(email(userId, categorizedPayments));
+
   });
 
-  it('unusal spending manages the collaboration of fetch, ' +
-    'categorize and email');
+
 });
 
