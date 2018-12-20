b02a4684a5fa82af41cf348194b6aa600cb0471b
AA Developer <developer@aa.com>
2018-12-20 17:07:45 -0600

    Unusual spending - implementation for categorize().

--- server/exercises/06-unusual-spending/categorize.js
+++ server/exercises/06-unusual-spending/categorize.js
@@ -1,25 +1,30 @@
+const calculateCategorizedPayments = payments => {
+  let categorizedPayments = [];
+
+  payments.forEach(payment => {
+    if (categorizedPayments.length > 0 && payment.category === categorizedPayments[categorizedPayments.length - 1].category) {
+      categorizedPayments[categorizedPayments.length - 1].amount += payment.amount;
+    } else {
+      categorizedPayments.push(payment);
+    }
+  });
+
+  return categorizedPayments;
+};
+
 export const categorize = payments => {
-  return [{
-    month: {
-      month: 12,
-      year: 2018
-    },
-    payments: [{
-      amount: 50,
-      category: 'coffee'
-    }, {
-      amount: 120,
-      category: 'grocery'
-    }]
-  }, {
-    month: {
-      month: 11,
-      year: 2018
-    },
-    payments: [{
-      amount: 130,
-      category: 'cable'
-    }]
-  }
-  ];
+  let categorizedPayments = [];
+
+  payments.forEach(payment => {
+    let categorizedPayment = {};
+
+    categorizedPayment = {
+      month: payment.month,
+      payments: calculateCategorizedPayments(payment.payments)
+    };
+
+    categorizedPayments.push(categorizedPayment);
+  });
+
+  return categorizedPayments;
 };
