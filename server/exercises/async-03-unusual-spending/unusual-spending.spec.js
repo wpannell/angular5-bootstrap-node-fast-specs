c91738937af227baaec1d8ec7ec3f84f4a818c06
AA Developer <developer@aa.com>
2018-12-21 15:48:10 -0600

    Unusual spending - async - design spec.

---
+++ server/exercises/async-03-unusual-spending/unusual-spending.spec.js
@@ -0,0 +1,8 @@
+describe('unusual spending', () => {
+  it('has a canary test', () => {
+    true.should.be.true();
+  });
+
+  it('successfully manages the collaboration of fetch, categorize and email');
+  it('failed the collaboration of fetch, categorize and email');
+});
