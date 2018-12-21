fdfa4e29f1f6b17b978bc0e9a17ed4552e15f7ec
AA Developer <developer@aa.com>
2018-12-21 16:28:16 -0600

    promise unusual spending

---
+++ server/exercises/async-03-unusual-spending/unusual-spending-async.spec.js
@@ -0,0 +1,46 @@
+import * as td from 'testdouble';
+import {Promise} from 'q';
+
+const userId = 'userId';
+const categorizedPayments = 'categorizedPayments';
+const payments = 'payments';
+
+const {replace, when, verify} = td;
+
+describe('unusual spending async', () => {
+  it('has a canary test', () => {
+    true.should.be.true();
+  });
+
+  const fetch = replace(require('./fetch'), 'fetch');
+  const categorize = replace(require('./categorize'), 'categorize');
+  const email = replace(require('./email'), 'email');
+
+  it('sucessfully manages the collaboration of fetch, categorize, email', () => {
+    when(fetch(userId)).thenReturn(new Promise((resolve, reject) => {
+      return resolve(payments);
+    }));
+
+    when(categorize(payments)).thenReturn(categorizedPayments);
+    const unusualSpending = require('./unusual-spending').unusualSpending;
+
+    return unusualSpending(userId).then(() => {
+      verify(email(userId, categorizedPayments));
+    });
+
+  });
+
+  it('on fail', () => {
+
+    when(fetch(userId)).thenReturn(new Promise((reject) => {
+      return reject('error');
+    }));
+    const unusualSpending = require('./unusual-spending').unusualSpending;
+
+    return unusualSpending(userId).catch((rejected) => {
+      rejected.message.should.equal('problem with fetc');
+    });
+
+  });
+});
+
---
+++ server/exercises/async-03-unusual-spending/unusual-spending.js
@@ -0,0 +1,15 @@
+import {categorize} from './categorize';
+import {fetch} from './fetch';
+import {email} from './email';
+
+export const unusualSpending = (id) => {
+
+  return fetch(id).then((payments) => {
+    const categorizedPayments = categorize(payments);
+    email(id, categorizedPayments);
+
+  }).catch((error) => {
+    throw new Error('problem with fetch');
+  });
+
+};
