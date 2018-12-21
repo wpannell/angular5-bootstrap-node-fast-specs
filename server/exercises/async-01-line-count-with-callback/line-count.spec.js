70b89b67e8ccb7feacaa0eb0610bcdab2a854cc6
wil pannell <wil.pannell@gmail.com>
2018-12-21 16:37:24 -0600

    Complete async line count exercise

--- server/exercises/async-01-line-count-with-callback/line-count.spec.js
+++ server/exercises/async-01-line-count-with-callback/line-count.spec.js
@@ -1,6 +1,23 @@
+import {lineCount} from './line-count';
+
+const filename = 'server/exercises/async-01-line-count-with-callback/line-count.js';
+const errorFile = 'is-not-there.js';
+
 describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+  it('for file —— line-count.js —— should be 13', (done) => {
+    lineCount(filename, null, (lines) => {
+      lines.should.equal(13);
+      done();
+    });
+  });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
-      'is-not-there.js');
+    'is-not-there.js', (done) => {
+
+    lineCount(errorFile, (msg) => {
+      msg.should.equal('problem reading file: ' + errorFile);
+      done();
+    }, null);
+  });
+
 });
--- server/exercises/async-02-line-count-with-promise/line-count.spec.js
+++ server/exercises/async-02-line-count-with-promise/line-count.spec.js
@@ -1,9 +1,22 @@
+import {lineCount} from './line-count';
+
+const filespec = 'server/exercises/async-02-line-count-with-promise/line-count.js';
+
 describe('line count can be checked', () => {
-  //const filespec = 'server/async-01-line-count-with-callback/line-count.js';
+  it('with the done parameter like with a callback', (done) => {
+    lineCount(filespec).then((numberOfLinesInTheFile) => {
+      numberOfLinesInTheFile.should.equal(14);
+      done();
+    });
+  });
 
-  it('with the done parameter like with a callback');
+  it('by returning the promise after .then', () => {
+    return lineCount(filespec).then((numberOfLinesInTheFile) => {
+      numberOfLinesInTheFile.should.equal(14);
+    });
+  });
 
-  it('by returning the promise after .then');
-
-  it('by using eventually or fulfilledWith');
+  it('by using eventually or fulfilledWith', () => {
+    return lineCount(filespec).should.be.fulfilledWith(14);
+  });
 });
