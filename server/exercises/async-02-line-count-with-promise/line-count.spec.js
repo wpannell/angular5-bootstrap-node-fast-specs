27033c5e298b8756ba840a603ae16c016e3173ab
AA Developer <developer@aa.com>
2018-12-21 14:33:22 -0600

    Line count - Promise - happy path unit tests.

--- server/exercises/async-02-line-count-with-promise/line-count.spec.js
+++ server/exercises/async-02-line-count-with-promise/line-count.spec.js
@@ -1,9 +1,26 @@
+import {lineCount} from './line-count';
+const file = 'server/exercises/async-02-line-count-with-promise/line-count.js';
+
 describe('line count can be checked', () => {
-  //const filespec = 'server/async-01-line-count-with-callback/line-count.js';
 
-  it('with the done parameter like with a callback');
+  it('with the done parameter like with a callback', (done) => {
+    lineCount(file).then((fileLineCount) => {
+      fileLineCount.should.equal(14);
+      done();
+    });
+  });
 
-  it('by returning the promise after .then');
+  it('by returning the promise after .then', () => {
+    return lineCount(file).then((fileLineCount) => {
+      fileLineCount.should.equal(14);
+    });
+  });
 
-  it('by using eventually or fulfilledWith');
+  it('by using eventually or fulfilledWith', () => {
+    return lineCount(file).should.eventually.equal(14);
+  });
+
+  it('by using eventually or fulfilledWith', () => {
+    return lineCount(file).should.be.fulfilledWith(14);
+  });
 });
