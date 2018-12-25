0c7439bfabea8e77aa62ca25c37452eb83c62ae5
AA Developer <developer@aa.com>
2018-12-25 16:48:24 -0600

    async-02-line-count-with-promise corrected relative path

--- server/exercises/async-02-line-count-with-promise/line-count.spec.js
+++ server/exercises/async-02-line-count-with-promise/line-count.spec.js
@@ -1,9 +1,27 @@
+import {lineCount} from './line-count';
+
+const filespec = 'server/exercises/async-02-line-count-with-promise/line-count.js';
+
 describe('line count can be checked', () => {
-  //const filespec = 'server/async-01-line-count-with-callback/line-count.js';
 
-  it('with the done parameter like with a callback');
+  it('with the done parameter like with a callback', () => {
+    lineCount(filespec).then((numberOfLinesInTheFile) => {
+      numberOfLinesInTheFile.should.equal(13);
 
-  it('by returning the promise after .then');
+    });
+  });
 
-  it('by using eventually or fulfilledWith');
+  it('by returning the promise after .then', () => {
+    return lineCount(filespec).then((numberOfLinesInTheFile) => {
+      numberOfLinesInTheFile.should.equal(14);
+    });
+  });
+
+  it('by using fulfilledWith', () => {
+    return lineCount(filespec).should.be.fulfilledWith(14);
+  });
+
+  it('by using eventually', () => {
+    return lineCount(filespec).should.eventually.equal(14);
+  });
 });
