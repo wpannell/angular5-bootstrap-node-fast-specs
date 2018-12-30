76e8d2256baa6b7a2f8dd09f8dc2f748772a78c8
AA Developer <developer@aa.com>
2018-12-29 21:35:58 -0600

    Line count with callback - success scenario.

--- server/exercises/async-01-line-count-with-callback/line-count.spec.js
+++ server/exercises/async-01-line-count-with-callback/line-count.spec.js
@@ -1,5 +1,14 @@
+import {lineCount} from './line-count';
+
+const file = 'server/exercises/async-01-line-count-with-callback/line-count.js';
+
 describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+  it('for file —— line-count.js —— should be 13', done => {
+    lineCount(file, null, totalLineCount => {
+      totalLineCount.should.equal(13);
+      done();
+    });
+  });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
       'is-not-there.js');
